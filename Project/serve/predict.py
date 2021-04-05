f0b32c78cc0b8cb7191151bd31317ce128076860
Viktor Begun <40863806+viktor-begun@users.noreply.github.com>
2021-04-05 22:59:47 +0200

    Updated "TODO".

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,8 +70,14 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    #data_X = None
+    #data_len = None
+    
+    # As we did in the "More testing" section of the "Sage Maker Projec.ipynb"
+    #test_review_words = review_to_words(test_review)
+    input_data_words = review_to_words(input_data)
+    #test_review_words, test_review_len = convert_and_pad(word_dict, test_review_words)
+    data_X, data_len = convert_and_pad(word_dict, input_data_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +93,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = model(data)
 
     return result
